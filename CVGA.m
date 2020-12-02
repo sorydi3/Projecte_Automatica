7462542089267b91c1b8548bfe7b4522696ea5f0
sorydi3 <ibrahimasory22@hotmail.com>
2020-12-02 20:18:23 +0000

    secod commit

---
+++ CVGA.m
@@ -0,0 +1,47 @@
+function CVGA(G)
+
+for i=1: size(G,1)
+    Min_gli(i) = prctile(G(i,:),2.5);
+    Max_gli(i) = prctile(G(i,:),97.5);
+end
+p=polyfit([110 180 300 400],[0 20 40 60],3);
+Max_gli = polyval(p,Max_gli);
+
+
+figure
+hold on
+%fill zone C
+X_pcmeno = [40 60 60 40]; Y_pcmeno = [0 0 20 20]; fill(X_pcmeno,Y_pcmeno,[1 1 0]);
+%fill zone D
+X_pdmeno = [40 60 60 40]; Y_pdmeno = [20 20 40 40]; fill(X_pdmeno,Y_pdmeno,[1 0.6 0]);
+%fill zone E
+X_pe = [40 60 60 40]; Y_pe = [40 40 60 60]; fill(X_pe,Y_pe,[1 0 0]);
+%fill zone B
+X_pbmeno = [20 40 40 20]; Y_pbmeno = [0 0 20 20]; fill(X_pbmeno,Y_pbmeno,[7/255 135/255 0/255]);
+%fill zone C
+X_pc = [20 40 40 20]; Y_pc = [20 20 40 40]; fill(X_pc,Y_pc,[7/255 135/255 0/255]);
+%fill zone D+
+X_pdpiu = [20 40 40 20]; Y_pdpiu = [40 40 60 60]; fill(X_pdpiu,Y_pdpiu,[1 0.6 0]);
+%fill zone A
+X_pa = [0 20 20 0]; Y_pa = [0 0 20 20]; fill(X_pa,Y_pa,[0 1 0]);
+%fill zone B+
+X_pbpiu = [0 20 20 0]; Y_pbpiu = [20 20 40 40]; fill(X_pbpiu,Y_pbpiu,[7/255 135/255 0/255]);
+%fill zone C+
+X_pcpiu = [0 20 20 0]; Y_pcpiu = [40 40 60 60]; fill(X_pcpiu,Y_pcpiu,[1 1 0]);
+text(10,10,'\textbf{A}','HorizontalAlignment','center','Interpreter','Latex','Fontsize',15);
+text(10,30,'\textbf{Upper B}','HorizontalAlignment','center','Interpreter','Latex' ,'Fontsize',15);
+text(10,50,'\textbf{Upper C}','HorizontalAlignment','center','Interpreter','Latex' ,'Fontsize',15);
+text(30,10,'\textbf{Lower B}','HorizontalAlignment','center','Interpreter','Latex' ,'Fontsize',15);
+text(30,30,'\textbf{B}','HorizontalAlignment','center','Interpreter','Latex' ,'Fontsize',15);
+text(30,50,'\textbf{Upper D}','HorizontalAlignment','center','Interpreter','Latex' ,'Fontsize',15);
+text(50,10,'\textbf{Lower C}','HorizontalAlignment','center','Interpreter','Latex' ,'Fontsize',15);
+text(50,30,'\textbf{Lower D}','HorizontalAlignment','center','Interpreter','Latex' ,'Fontsize',15);
+text(50,50,'\textbf{E}','HorizontalAlignment','center','Interpreter','Latex' ,'Fontsize',15);
+set(gca,'YTick',[0 20 40 60])
+set(gca,'XTick',[0 20 40 60])
+set(gca,'XTickLabel',{'>110' '90' '70' '<50'})
+set(gca,'YTickLabel',{'<110' '180' '300' '>400'})
+scatter(min(max(110 - Min_gli,0),60),min(max(Max_gli,0),60),'ow','filled');
+xlabel('Minimum BG'),ylabel('Maximum BG')
+grid on
+box on
---
+++ model_hovorkaSS.m
@@ -0,0 +1,65 @@
+function F = model_hovorkaSS(x, u, params)
+% Assignem noms adients a les variables que formen un pacient
+k12 = params(1);
+ka1 = params(2);
+ka2 = params(3);
+ka3 = params(4);
+ke = params(5);
+VI = params(6);
+Vg = params(7);
+SfIT = params(8);
+SfID = params(9);
+SfIE = params(10);
+EGP0 = params(11);
+F01 = params(12);
+tmaxI = params(13);
+BW = params(14);
+kb1 = params(15);
+kb2 = params(16);
+kb3 = params(17);
+tau = params(18);
+Ag = params(19);
+tmaxG = params(20);
+
+% Taxa d’absorció de carbohidrats constant i zero
+Ug = 0;
+
+% Assigneu variables amb els noms corresponents a tots els elements del vector x
+S1 = x(1);
+S2 = x(2);
+I = x(3);
+x1 = x(4);
+x2 = x(5);
+x3 = x(6);
+Q1 = x(7);
+Q2 = x(8);
+IG = x(9);
+G1=x(10);
+G2=x(11)
+
+if (IG >= 4.5)
+    F01c = F01;
+else
+    F01c = F01*IG/4.5;
+end
+
+if (IG >= 9)
+    FR = 0.003*(IG - 9)*Vg;
+else
+    FR = 0;
+end
+
+% Implementeu les diferents equacions del model d’Hovorka
+F(1) = u(1) - S1/tmaxI; % ·S1(t) = u(t)-S1(t)/tmaxI
+F(2) = (S1 - S2)/tmaxI; % ·S2(t) = (S1(t) - S2(t))/tmaxI
+F(3) = S2/(tmaxI*VI) - ke*I; % ·I(t) = S2(t)/(tmaxI*VI) - ke*I(t)
+F(4) = -ka1*x1 + kb1*I; % ·x1(t) = -ka1*x1(t) + kb1*I(t)
+F(5) = -ka2*x2 + kb2*I; % ·x2(t) = -ka2*x2(t) + kb2*I(t)
+F(6) = -ka3*x3 + kb3*I; % ·x3(t) = -ka3*x3(t) + kb3*I(t)
+F(7) = -x1*Q1 + k12*Q2 - F01c - FR + Ug + EGP0*(1-x3); % ·Q1(t) = -x1(t)*Q1(t) + k12*Q2(t) - F01c(t) - FR(t) + Ug(t) + EGP0*(1-x3(t))
+F(8) = x1*Q1 - k12*Q2 - x2*Q2;% ·Q2(t) = x1(t)*Q1(t) - k12*Q2(t) - x2(t)*Q2(t)
+F(9) = (1/tau)*( Q1/Vg - IG ); % ·IG(t) = (1/tau)*( Q1(t)/Vg - IG(t) )
+F(10) = (-G1/tmaxG)+u(2);
+F(11) = (G1-G2)/tmaxG;
+F = F';
+end
