dbbed06039592bc05641f25f3fcb123f466a056b
Martina <75387763+Martinarod13@users.noreply.github.com>
2021-01-10 18:35:39 +0100

    Add files via upload

--- model_hovorkaSS.m
+++ model_hovorkaSS.m
@@ -5,7 +5,6 @@ ka1 = params(2);
 ka2 = params(3);
 ka3 = params(4);
 ke = params(5);
-
 VI = params(6);
 Vg = params(7);
 SfIT = params(8);
@@ -35,8 +34,6 @@ x3 = x(6);
 Q1 = x(7);
 Q2 = x(8);
 IG = x(9);
-G1=x(10);
-G2=x(11)
 
 if (IG >= 4.5)
     F01c = F01;
@@ -51,16 +48,15 @@ else
 end
 
 % Implementeu les diferents equacions del model d’Hovorka
-F(1) = u(1) - S1/tmaxI; % ·S1(t) = u(t)-S1(t)/tmaxI
+F(1) = u - S1/tmaxI; % ·S1(t) = u(t)-S1(t)/tmaxI
 F(2) = (S1 - S2)/tmaxI; % ·S2(t) = (S1(t) - S2(t))/tmaxI
 F(3) = S2/(tmaxI*VI) - ke*I; % ·I(t) = S2(t)/(tmaxI*VI) - ke*I(t)
 F(4) = -ka1*x1 + kb1*I; % ·x1(t) = -ka1*x1(t) + kb1*I(t)
 F(5) = -ka2*x2 + kb2*I; % ·x2(t) = -ka2*x2(t) + kb2*I(t)
 F(6) = -ka3*x3 + kb3*I; % ·x3(t) = -ka3*x3(t) + kb3*I(t)
 F(7) = -x1*Q1 + k12*Q2 - F01c - FR + Ug + EGP0*(1-x3); % ·Q1(t) = -x1(t)*Q1(t) + k12*Q2(t) - F01c(t) - FR(t) + Ug(t) + EGP0*(1-x3(t))
-F(8) = x1*Q1 - k12*Q2 - x2*Q2;% ·Q2(t) = x1(t)*Q1(t) - k12*Q2(t) - x2(t)*Q2(t)
+F(8) = x1*Q1 - k12*Q2 - x2*Q2; % ·Q2(t) = x1(t)*Q1(t) - k12*Q2(t) - x2(t)*Q2(t)
 F(9) = (1/tau)*( Q1/Vg - IG ); % ·IG(t) = (1/tau)*( Q1(t)/Vg - IG(t) )
-F(10) = (-G1/tmaxG)+u(2);
-F(11) = (G1-G2)/tmaxG;
+
 F = F';
 end
