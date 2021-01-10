e869fbe7d6f8a4f077c70a08f7198d82f7b48698
Martina <75387763+Martinarod13@users.noreply.github.com>
2021-01-10 16:23:51 +0100

    Add files via upload

--- OL.m
+++ OL.m
@@ -1,7 +1,11 @@
-function u = OL(i,u,open_loop)
+function [u,open_loop,params] = OL(i,u,open_loop,params)
 % Terapia basal + bolus
 if ~isempty(open_loop.num_menjars) && open_loop.index_menjar <= length(open_loop.grams_cho)
+    %cho = open_loop.temps_cho(open_loop.index_menjar)*60;
+    %X = sprintf('%d == %d',i , cho);
+    %disp(X);
     if i == open_loop.temps_cho(open_loop.index_menjar)*60
+        params.meal_time = params.meal_time + 1;
         u(2) = open_loop.grams_cho(open_loop.index_menjar);
         open_loop.bolus = (u(2)/open_loop.CR)*1000; % mU
         u(1) = open_loop.basal*1000/60 + open_loop.bolus; % mU/min
--- mainCL.m
+++ mainCL.m
@@ -11,20 +11,21 @@ controller.index_menjar = 1*(~isempty(controller.grams_cho));
 
 hist_states = [];
 u = zeros(1,2);
+params.meal_time = 0;
 for i = 1:controller.sim_time
-    
     if strcmp(controller.tipus,'OL') == 1
-        u = OL(i,u,controller);
+        [u,controller,params] = OL(i,u,controller,params);
     elseif strcmp(controller.tipus,'PID') == 1
         u = PID(i,u,controller);
     end
-    
     [~,Xhov] = ode45(@(t,x) model_hovorka(t, x, u, controller.pacient), [i i+1], Xkm1);
     Xkm1 = Xhov(end,:)';
     hist_states = [hist_states; Xkm1'];
 end
 
 % Mostrem resultats per pantalla
+i = size(1:controller.sim_time);
+j = hist_states(:,9)*18;
 plot((1:controller.sim_time)./60,hist_states(:,9)*18); hold on;
 xlim([0 24]);
 limitsY = get(gca,'ylim');
